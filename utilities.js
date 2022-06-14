47d01bd0799271ffdcd2e1ab8b1eff017eddb4fd
Ahmed Moussa <ahmmoussa22@gmail.com>
2022-06-14 11:29:53 -0400

    Updated gitignore for Typescript.

--- utilities.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.floor_tf = void 0;
-var floor_tf = function (frame, timestamp) { return timestamp - timestamp % frame; };
-exports.floor_tf = floor_tf;
